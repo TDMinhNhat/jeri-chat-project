a1bc39a8237cc02f52be021a47856371b65cdd66
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-10-31 11:04:34 +0700

    Do add/modify/retrieve address

--- user-service/src/main/java/io/github/tdminhnhat/repository/AddressRepository.java
+++ user-service/src/main/java/io/github/tdminhnhat/repository/AddressRepository.java
@@ -4,6 +4,10 @@ import io.github.tdminhnhat.entity.Address;
 import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
+
+    Optional<Address> findByUser_Id(Long id);
 }
--- user-service/src/main/java/io/github/tdminhnhat/service/impl/UserServiceImpl.java
+++ user-service/src/main/java/io/github/tdminhnhat/service/impl/UserServiceImpl.java
@@ -1,10 +1,13 @@
 package io.github.tdminhnhat.service.impl;
 
+import io.github.tdminhnhat.entity.Address;
 import io.github.tdminhnhat.entity.User;
 import io.github.tdminhnhat.exception.QueryNotFoundException;
+import io.github.tdminhnhat.mapper.AddressMapper;
 import io.github.tdminhnhat.mapper.UserMapper;
 import io.github.tdminhnhat.model.dto.UserDTO;
 import io.github.tdminhnhat.model.vo.UserVO;
+import io.github.tdminhnhat.repository.AddressRepository;
 import io.github.tdminhnhat.repository.UserRepository;
 import io.github.tdminhnhat.service.UserService;
 import io.micronaut.http.multipart.CompletedFileUpload;
@@ -13,37 +16,60 @@ import lombok.RequiredArgsConstructor;
 
 import java.io.InputStream;
 import java.util.List;
+import java.util.UUID;
 
 @Singleton
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final AddressRepository addressRepository;
     private final UserMapper userMapper;
+    private final AddressMapper addressMapper;
 
     @Override
     public UserVO add(UserDTO request) {
         User user = userMapper.toEntity(request);
-        user.setCode(generateUserCode());
-        return userMapper.toVO(userRepository.save(user));
+        Address address = addressRepository.save(addressMapper.toEntity(request.address()));
+        user.setAddress(address);
+
+        user.setCode(generateUserCode(request.firstName(), request.lastName()));
+        UserVO userVO = userMapper.toVO(userRepository.save(user));
+        userVO.setAddress(addressMapper.toVO(address));
+        return userVO;
     }
 
     @Override
     public UserVO update(Long id, UserDTO request) {
         User user = userRepository.findById(id).orElseThrow(() -> new QueryNotFoundException("User Not Found"));
-        return userMapper.toVO(userRepository.save(user));
+        Address address = addressRepository.findByUser_Id(user.getId()).orElseThrow(() -> new QueryNotFoundException("Address Not Found"));
+        address = addressMapper.toEntity(request.address());
+        user.setAddress(address);
+        
+        UserVO userVO = userMapper.toVO(userRepository.save(user));
+        userVO.setAddress(addressMapper.toVO(address));
+        return userVO;
     }
 
     @Override
     public UserVO delete(Long id) {
         User user = userRepository.findById(id).orElseThrow(() -> new QueryNotFoundException("User Not Found"));
         user.setDeleted(true);
-        return userMapper.toVO(userRepository.save(user));
+        Address address = addressRepository.findByUser_Id(user.getId()).orElseThrow(() -> new QueryNotFoundException("Address Not Found"));
+        address.setDeleted(true);
+
+        UserVO userVO = userMapper.toVO(userRepository.save(user));
+        userVO.setAddress(addressMapper.toVO(addressRepository.save(address)));
+        return userVO;
     }
 
     @Override
     public UserVO getById(Long id) {
-        return userMapper.toVO(userRepository.findById(id).orElseThrow(() -> new QueryNotFoundException("User Not Found")));
+        User user = userRepository.findById(id).orElseThrow(() -> new QueryNotFoundException("User Not Found"));
+        UserVO userVO = userMapper.toVO(user);
+        Address address = addressRepository.findByUser_Id(user.getId()).orElseThrow(() -> new QueryNotFoundException("Address Not Found"));
+        userVO.setAddress(addressMapper.toVO(address));
+        return userVO;
     }
 
     @Override
@@ -65,7 +91,7 @@ public class UserServiceImpl implements UserService {
         return List.of();
     }
 
-    private String generateUserCode() {
-        return "";
+    private String generateUserCode(String firstName, String lastName) {
+        return UUID.fromString(firstName + " " + lastName).toString();
     }
 }
